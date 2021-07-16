cc34893a489c8813aace2433701e0e8669004894
James Falade <jamesthedove@gmail.com>
2021-07-16 11:57:33 +0100

    updates

--- dist/api-client.js
+++ dist/api-client.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
 exports.default = axios_1.default.create({
-    baseURL: "https://wallets.blusalt.net",
+    baseURL: "https://wallets.blusalt.net" || process.env.BLUSALT_API_URL,
     headers: {
         Accept: 'application/json',
         'x-api-key': process.env.BLUSALT_API_KEY
--- src/api-client.ts
+++ src/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://wallets.blusalt.net",
+    baseURL: "https://wallets.blusalt.net" || process.env.BLUSALT_API_URL,
     headers: {
         Accept: 'application/json',
         'x-api-key': process.env.BLUSALT_API_KEY
