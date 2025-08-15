ad7947bf399c4c62cb993e84b015b0a9f8f9c64a
James Falade <jamesthedove@gmail.com>
2025-08-15 19:55:50 +0100

    chore: handle errors

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getClient = (apiKey?: string) => {
-    return  axios.create({
-        baseURL: process.env.BLUSALT_API_URL || "https://wallets.blusalt.net",
-        headers: {
-            Accept: 'application/json',
-            'x-api-key': apiKey || process.env.BLUSALT_API_KEY
-        }
-    });
-
-}
+  return axios.create({
+    baseURL: process.env.BLUSALT_API_URL || "https://wallets.blusalt.net",
+    headers: {
+      Accept: "application/json",
+      "x-api-key": apiKey || process.env.BLUSALT_API_KEY,
+    },
+    validateStatus: (status) => {
+      return true;
+    },
+  });
+};
--- src/error.ts
+++ src/error.ts
@@ -1,5 +1,13 @@
-export class BlusaltError extends Error{
-    constructor(message: string) {
-        super(message);
-    }
+export class BlusaltError extends Error {
+  errorCode: string;
+  error: string;
+  constructor(response: {
+    message: string;
+    error_code: string;
+    error: string;
+  }) {
+    super(response.message);
+    this.errorCode = response.error_code;
+    this.error = response.error;
+  }
 }
